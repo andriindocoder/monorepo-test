c5a0c8760283a84e2a55ccb44205321e75998e0a
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-02-02 21:34:39 +0700

    9.34 hardcoder

--- packages/simple-express-server/server.ts
+++ packages/simple-express-server/server.ts
@@ -14,7 +14,7 @@ app.use((_req, res, next) => {
 
 app.get("/", (_req, res) => {
   const responseData: QueryPayload = {
-    payload: _.snakeCase("Server data returned successfully server update again again again"),
+    payload: _.snakeCase("Server data returned successfully server update again again"),
   };
 
   res.json(responseData);
