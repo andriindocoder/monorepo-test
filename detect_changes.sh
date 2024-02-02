9f7179bc3635b83c374daba97c8f9b657bffa0a8
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-02-02 16:38:09 +0700

    Run again

--- detect_changes.sh
+++ detect_changes.sh
@@ -5,7 +5,7 @@ BASE_BRANCH="main"
 
 # # Get a list of changed files compared to the BASE_BRANCH
 # CHANGED_FILES="$(git diff --name-only HEAD $(git merge-base HEAD $BASE_BRANCH))"
-CHANGED_FILES=$(git diff --name-only)
+CHANGED_FILES="$(git diff --name-only)"
 # Define the sub-projects in the monorepo
 declare -a PROJECTS=("packages/simple-express-server" "packages/simple-react-app" "packages/simple-shared-data")
 
