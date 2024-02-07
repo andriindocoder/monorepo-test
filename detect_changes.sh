5e0eace7f3bd166c03b2db3ffc33b7ac3f933afc
Andri Wicaksono <andri.wicaksono@appfuxion.id>
2024-02-07 11:35:45 +0700

    Update microservice-A

--- detect_changes.sh
+++ detect_changes.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Base branch to compare (e.g., master)
-BASE_BRANCH="main"
 
-# # Get a list of changed files compared to the BASE_BRANCH
-# CHANGED_FILES="$(git diff --name-only HEAD $(git merge-base HEAD $BASE_BRANCH))"
-# CHANGED_FILES="$(git diff --name-only)"
-# CHANGED_FILES="$(git diff --name-only origin/main...HEAD)"
-# CHANGED_FILES=$(git diff --name-only "origin/$BASE_BRANCH"...HEAD)
 CHANGED_FILES=$(git diff --name-only HEAD~1..HEAD)
+
 # Define the sub-projects in the monorepo
 declare -a PROJECTS=("packages/simple-express-server" "packages/simple-react-app" "packages/simple-shared-data")
 
